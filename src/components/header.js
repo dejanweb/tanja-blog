c70952cd712e8c96fd6ba013fc7831dfaff1b258
dejan bulatic <dejanbulatic@outlook.com>
2019-11-15 17:50:54 +0100

    header styled

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import HeaderStyles from './header.module.scss'
 import './bootstrap.min.css'
 
 
 const Header = () => {
     return (
-        <header>
-            <div className="container">
-                <h1>Tanja Dinic</h1>
-                <ul>
+        <header className={HeaderStyles.headstyle}>
+            <div className="container text-center">
+                <h1 className={HeaderStyles.heroheader}>Tanja Dinic</h1>
+                <ul className="list-unstyled d-flex justify-content-center pb-2">
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link className={HeaderStyles.link} to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link className={HeaderStyles.link} to="/about">About</Link>
                     </li>
                 </ul>
             </div>
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import Sidebar from './sidebar'
-import './layout.css'
+
 import './bootstrap.min.css'
 
 const Layout = (props) => {
